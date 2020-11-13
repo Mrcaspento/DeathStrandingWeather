37a8ffa879dd4310594e41a508277ea9c8257999
Caspento <Mrcaspento@gmail.com>
2020-11-12 22:53:56 -0500

    working on storage

--- weather.js
+++ weather.js
@@ -1,61 +1,93 @@
-var today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyyy = today.getFullYear();
-var today = mm + '/' + dd + '/' + yyyy;
 // This is my API key
-let apiKey = "5e329d9e34b0e4196b874d9df878fa73";
+let apiKey = "&units=imperial&appid=e222da09846714641d1de2e70cf4f020";
 //grabing the text from the input box
-var city = $('.city-input').val();
 var searchBtn = $('#add-city');
+
+
 
 var searchHistory = $('.cityHistory');
 var iconEl = $('.weathericon');
 var tody = $('.currentDate');
 var cardRow = $('.card-row')
 var cityNameEl = $('.cityName')
-var currentDateEl = $('.currentDate') 
+var currentDateEl = $('.currentDate')
 var citys = [];
-
+let city = $('#cityTerm').val()
 
 
 // work on the the JSON local storage 
 
+//$.getJSON(queryURL,function(data){
+//console.log(data);
+//})
+
+
+$('#cityTerm').keypress(function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $('#SearchBtn').click();
+
+    }
+});
+
+$('#searchBtn').click(function () {
+    city = $('#cityTerm').val();
+    $('#cityTerm').val("");
+
+    const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+        })
+        .then(function (response) {
+
+            console.log(response)
+
+            console.log(response.name)
+            console.log(response.weather[0].icon)
+            let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            console.log(Math.floor(tempF))
+
+            console.log(response.main.humidity)
+
+            console.log(response.wind.speed)
+
+            getCurrentConditions(response);
+            getCurrentForecast(response);
+            makeList();
+        })
+
+})
+
+function displayCityInfo() {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        $('#buttons-view').text(JSON.stringify(response));
+    })
+
+}
 
 
 
 function renderCityBtn() {
     $('#buttons-view').empty(); // important to stop buttons from repeating
     for (var i = 0; i < citys.length; i++); {
-        var cityArry = citys[i]
-        
-        cityArry.addClass('city','btn btn-primary'); // added a data-atribute
-        cityArry.attr('city-name', citys[i]);//provided the intial button text
-        cityArry.text(citys[i]);//added the button to the HTML
-        $('#buttons-view').append(cityArry);
+        var cityBtn = $('<button>');
+        cityBtn.addClass("city");
+        cityBtn.attr("city-name", citys[i]);
+        cityBtn.text(citys[i]);
+        $('#buttons-view').append(cityBtn);
     }
 }
-// the function handles events where one button is clicked
-searchBtn.click(function(event) {
-    event.preventDefault();
-    if(city.val()=== ""){
-        alert("Please enter a city")
-    }
-    searchBtn
-    // grabs the input from the textbox
-    citys.push(city);// the city from the textbox is then added to the array
-    renderCityBtn();// handles the processing of the city array
-})
 
 
 
-// this the urls needed to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=5e329d9e34b0e4196b874d9df878fa73" + "&units=metric";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    $('.temp').text("temperture", response)
-})
+$(document).on('click', '.city', displayCityInfo);
 renderCityBtn();
+
+
